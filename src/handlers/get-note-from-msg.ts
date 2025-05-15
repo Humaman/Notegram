78f8e37b0541a5386a4ce3d0c1692a3853cc91e6
Nikita Yurash <g.doc.rus@gmail.com>
2025-05-16 01:30:03 +0300

    (hotfix) getNoteFromMsg after update db

--- src/handlers/get-note-from-msg.ts
+++ src/handlers/get-note-from-msg.ts
@@ -5,8 +5,8 @@ export function getNoteFromMsg(msg: any): NoteCreateInput {
     messageId: String(msg.message_id),
     text: msg.text ?? undefined,
     caption: msg.caption ?? undefined,
-    images: msg.photo ? msg.photo.at(-1)?.file_id : undefined,
-    videos: msg.video ? msg.video.file_id : undefined,
+    image: msg.photo ? msg.photo.at(-1)?.file_id : undefined,
+    video: msg.video ? msg.video.file_id : undefined,
     audio: msg.audio ? msg.audio.file_id : undefined,
     doc: msg.document ? msg.document.file_id : undefined,
   };
