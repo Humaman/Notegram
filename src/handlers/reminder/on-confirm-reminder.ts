c3971ed95bc63143c02fd3ef4daf99b99c357fe8
Nikita Yurash <g.doc.rus@gmail.com>
2025-05-17 01:54:35 +0300

    Prettify text

--- src/handlers/reminder/on-confirm-reminder.ts
+++ src/handlers/reminder/on-confirm-reminder.ts
@@ -4,26 +4,32 @@ import { CustomContext } from '../../types/custom-context.interface';
 
 export async function onConfirmReminder(ctx: CustomContext) {
   const reminderId: number = Number(ctx.match[1]);
-  const reminder = await prisma.reminder.update({
-    where: { id: reminderId },
-    data: { status: ReminderStatus.CONFIRMED },
-  });
-
-  const text = ctx.callbackQuery?.message?.text ?? ctx.callbackQuery?.message?.caption;
-
   const messageId = ctx.callbackQuery.message.message_id;
 
+  const note = ctx.callbackQuery?.message?.text ?? ctx.callbackQuery?.message?.caption;
+  let text;
+
+  let reminder = await prisma.reminder.findUnique({ where: { id: reminderId } });
+
+  if (reminder.status === ReminderStatus.CONFIRMED) {
+    text =
+      note +
+      `\n✔️ Это напоминание уже было подтверждено: ${reminder.updated_at.toLocaleString('ru-RU')}`;
+  } else {
+    reminder = await prisma.reminder.update({
+      where: { id: reminderId },
+      data: { status: ReminderStatus.CONFIRMED },
+    });
+
+    text = note + `\n✔️ Подтверждено: ${reminder.updated_at.toLocaleString('ru-RU')}`;
+  }
+
   if (ctx.callbackQuery?.message?.text) {
-    return await ctx.api.editMessageText(
-      ctx.chat.id,
-      messageId,
-      text + '\n' + `✔️ Подтверждено: ${reminder.updated_at.toLocaleString('ru-RU')}`,
-      { reply_markup: null },
-    );
+    return await ctx.api.editMessageText(ctx.chat.id, messageId, text, { reply_markup: null });
   }
   if (ctx.callbackQuery?.message?.caption) {
     return await ctx.api.editMessageCaption(ctx.chat.id, messageId, {
-      caption: text + '\n' + `✔️ Подтверждено: ${reminder.updated_at.toLocaleString('ru-RU')}`,
+      caption: text,
       reply_markup: null,
     });
   }
