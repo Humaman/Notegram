40bc7f942b3067b66b1a8a52ca7dfd41a87bc556
Nikita Yurash <g.doc.rus@gmail.com>
2025-05-20 00:40:36 +0300

    fix PrismaGetUserMiddleware

--- src/telegram/bot-middleware/prisma-get-user.middleware.ts
+++ src/telegram/bot-middleware/prisma-get-user.middleware.ts
@@ -1,28 +1,19 @@
 import { NextFunction } from 'grammy';
 
 import { createUser } from '../../common/create-user';
-import prisma from '../common/prisma';
+import prisma from '../../common/prisma';
 import { CustomContext } from '../types/custom-context.interface';
 
 export async function PrismaGetUserMiddleware(
   ctx: CustomContext,
-  next: NextFunction, // is an alias for: () => Promise<void>
+  next: NextFunction,
 ): Promise<void> {
-  if (!ctx.session.user) {
+  if (!ctx?.session?.user) {
     const tgId = String(ctx.chatId);
-    if (!tgId) {
-      console.error('\x1b[31mTelegram ID is missing in the message context\x1b[0m');
-      return;
-    }
+    let user = await prisma.user.findUnique({ where: { tg_id: tgId } });
 
-    let user = await prisma.user.findUnique({
-      where: { tg_id: tgId },
-    });
-
-    if (!user) {
-      user = await createUser(tgId);
-      ctx.session.user = user;
-    }
-    await next();
+    if (!user) user = await createUser(tgId);
+    ctx.session.user = user;
   }
+  await next();
 }
